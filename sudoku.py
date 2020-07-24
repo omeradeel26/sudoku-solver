a35f64deb001330761f9f421225dc32f00567289
omeradeel26 <57784751+omeradeel26@users.noreply.github.com>
2020-07-23 21:35:19 -0400

    added libraries + random sudoko creater

--- sudoku.py
+++ sudoku.py
@@ -1,62 +1,74 @@
-board = [[5,3,0,0,7,0,0,0,0],
-         [6,0,0,1,9,5,0,0,0],
-         [0,9,8,0,0,0,0,6,0],
-         [8,0,0,0,6,0,0,0,3],
-         [4,0,0,8,0,3,0,0,1],
-         [7,0,0,0,2,0,0,0,6],
-         [0,6,0,0,0,0,2,8,0],
-         [0,0,0,4,1,9,0,0,5],
-         [0,0,0,0,8,0,0,7,9]]
+import random
+import  pygame
 
-def print_out(bo):
-    for i in range(len(bo)): 
-        for j in range(len(bo[0])):
+pygame.init()
+
+screen =  pygame.display.set_mode((800,600))
+
+def random_generate():
+    grid = [[0 for x in range(9)] for y in range(9)]
+    row = random.randrange(9)
+    col = random.randrange(9)
+    num = random.randrange(1,10)
+    for i in range(20): 
+        while not validify(grid,num, (row,col)) or grid[row][col] != 0:
+            row = random.randrange(9)
+            col = random.randrange(9)
+            num = random.randrange(1,10)
+        grid[row][col] = num
+    return grid
+
+def print_out(grid):
+    for i in range(len(grid)): 
+        for j in range(len(grid[0])):
             if j%3 ==  0 and j != 0: 
                 print("| ", end="")
 
             if i%3 == 0 and i != 0 and j == 0:
                 print("---------------------")
 
-            print(bo[i][j],end=" ")
+            print(grid[i][j],end=" ")
 
-            if j == len(bo[0])-1:
+            if j == len(grid[0])-1:
                 print('')
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+def find_empty(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
                 return (i, j)
     return False
 
 
-def validify(bo, num, loc):
-    for i in range(len(bo[0])):
-        if num == bo[loc[0]][i]:
+def validify(grid, num, loc):
+    for i in range(len(grid[0])):
+        if num == grid[loc[0]][i]:
             return False
-    for i in range(len(bo[0])):
-        if num == bo[i][loc[1]]:
+    for i in range(len(grid[0])):
+        if num == grid[i][loc[1]]:
             return False 
     for i in range((loc[0]//3)*3,(loc[0]//3)*3 +3):
         for j in range((loc[1]//3)*3,(loc[1]//3)*3+3):
-            if num == bo[i][j]:
+            if num == grid[i][j]:
                 return False
     return True
 
-def recurse(bo):
+def recurse(grid):
     # doesnt find anything
-    if not find_empty(bo):
+    if not find_empty(grid):
         return True #if solution is done 
     else:
-        position = find_empty(bo)#finds empty position (row, col)
+        position = find_empty(grid)#finds empty position (row, col)
         for i in range(1,10): #goes through combination of 9
-            if validify(bo, i, position): #if the combo follows the rule in the empty location
-                bo[position[0]][position[1]] = i  #new location becomes the number
-                if recurse(bo): #recurses through function (will return True if combo is done)
+            if validify(grid, i, position): #if the comgrid follows the rule in the empty location
+                grid[position[0]][position[1]] = i  #new location becomes the number
+                if recurse(grid): #recurses through function (will return True if combo is done)
                     return True #if solution is done
-                bo[position[0]][position[1]] = 0
+                grid[position[0]][position[1]] = 0
     return False #ends if no possible combination
 
+
+board = random_generate()
 
 if recurse(board):
     print_out(board)
